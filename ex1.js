1a2eb9c5440dbfafc4e1a201a9a67c4fe74c7f62
Pattharanan <s6406021622052@email.kmutnb.ac.th>
2024-12-25 11:02:28 +0700

    7

--- ex1.js
+++ ex1.js
@@ -1,8 +1,17 @@
-function check() {
-    for(index = 0; index < value1.length; index++)
+function check(value1,value2,value3){
+    var returns ;
+    for(var i = 0 ; i < value1.length ; i++){
+        if(value1[0] === value2[i]){
+           returns = value2[i];
+        } 
+        else if(value1[0] === value3[i]){
+            returns = value3[i];
+         } 
+    }
+   return returns;
 }
+let values1 = ['Apple',1,false];
+let values2 = ['Fires',2,true];
+let values3 = ['Mars',9,'Apple'];
 
-
-let value1= ['Apple', 1, false];
-let value2= ['Fries', 2, true];
-let value3= ['Mars', 9, 'Apple'];
+console.log(check(values1,values2,values3));
