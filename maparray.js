725bc4a92905b580c364d29832282664cd62ff9e
Pattharanan <s6406021622052@email.kmutnb.ac.th>
2024-12-25 09:00:35 +0700

    2

---
+++ maparray.js
@@ -0,0 +1,7 @@
+const finalparticipants = ["Taylor", "Donald", "Don", "Natasha", "Bobby"];
+
+const announcements = finalparticipants.map((member) => {
+    return member + " joined the constest.";
+});
+
+console.log(announcements);
